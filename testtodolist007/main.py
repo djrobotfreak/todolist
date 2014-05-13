d48c6cfa11c7cc7842ae76bdc4f663536c082a0a
Derek Wene <djrobotfreak@gmail.com>
2014-05-13 14:52:18 -0500

    fixed the html with python

--- testtodolist007/main.py
+++ testtodolist007/main.py
@@ -14,12 +14,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
-        
+        template = JINJA_ENVIRONMENT.get_template('test.html')
+        template_values = {}
+        self.response.write(template.render(template_values))
+
 print "Content-type: text/html"
 print
 with open('test.html') as f:
