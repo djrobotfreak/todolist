2cfe4139335b8ed7c9939e90296b29f2eb7d1b61
Derek Wene <djrobotfreak@gmail.com>
2014-05-13 17:51:20 -0500

    stupid

--- testtodolist007/controller.py
+++ testtodolist007/controller.py
@@ -27,7 +27,7 @@ class listItem(ndb.Model):
                                    IOS_CLIENT_ID],
                audiences=[ANDROID_AUDIENCE])
 
-class RESTfulPart(remote.Service):
+class RESTApi(remote.Service):
     @endpoints.method(message_types.VoidMessage, Response,
                      path='getlist', http_method='GET',
                      name='listItem.getList')
@@ -37,78 +37,11 @@ class RESTfulPart(remote.Service):
        except (IndexError, TypeError):
            raise endpoints.NotFoundException('coudnt query correctly' %
                                              (request.id,))
-
-    """@endpoints.method(message_types.VoidMessage, Response,
-                  path='hellogreeting/{times}', http_method='POST',
-                  name='listItem.addItem')
-    def greetings_multiply(self, request):
-        newListItem = listItem(title = request.message, checked=false)
-        return Response(message=request.message)"""
-"""
-
-
-class GreetingCollection(messages.Message):
-    """Collection of Greetings."""
-    items = messages.MessageField(Greeting, 1, repeated=True)
-
-
-STORED_GREETINGS = GreetingCollection(items=[
-    Greeting(message='hello world!'),
-    Greeting(message='goodbye world!'),
-    Greeting(message='Derek says howdy'),
-    Greeting(message ='Hi Jake... Meow'),
-])
-
-
-@endpoints.api(name='helloworld', version='v1',
-               allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
-                                   IOS_CLIENT_ID],
-               audiences=[ANDROID_AUDIENCE])
-class HelloWorldApi(remote.Service):
-    """Helloworld API v1."""
-
-    MULTIPLY_METHOD_RESOURCE = endpoints.ResourceContainer(
-            Greeting,
-            times=messages.IntegerField(2, variant=messages.Variant.INT32,
-                                        required=True))
-
-    @endpoints.method(MULTIPLY_METHOD_RESOURCE, Greeting,
-                      path='hellogreeting/{times}', http_method='POST',
-                      name='greetings.multiply')
-    def greetings_multiply(self, request):
-        return Greeting(message=request.message * request.times)
-
-    @endpoints.method(message_types.VoidMessage, Greeting,
+    @endpoints.method(message_types.VoidMessage, Response,
                       path='derekgreeting', http_method='GET',
-                      name='greetings.howdyDerek')
+                      name='todolist.howdyDerek')
     def greetings_derek(self, request):
-        return Greeting(message='howdy derek')
+        return Response(message='howdy derek')
 
-    @endpoints.method(message_types.VoidMessage, GreetingCollection,
-                      path='hellogreeting', http_method='GET',
-                      name='greetings.listGreeting')
-    def greetings_list(self, unused_request):
-        return STORED_GREETINGS
 
-    ID_RESOURCE = endpoints.ResourceContainer(
-            message_types.VoidMessage,
-            id=messages.IntegerField(1, variant=messages.Variant.INT32))
-
-    @endpoints.method(ID_RESOURCE, Greeting,
-                      path='hellogreeting/{id}', http_method='GET',
-                      name='greetings.getGreeting')
-    def greeting_get(self, request):
-        try:
-            return STORED_GREETINGS.items[request.id]
-        except (IndexError, TypeError):
-            raise endpoints.NotFoundException('Greeting %s not found.' %
-                                              (request.id,))
-
-    @endpoints.method(message_types.VoidMessage, Greeting,
-                      path='hellogreeting/authed', http_method='POST',
-                      name='greetings.authed')
-    def greeting_authed(self, request):
-        current_user = endpoints.get_current_user()
-        email = (current_user.email() if current_user is not None
-                 else 'Anonymous')
-        return Greeting(message='hello %s' % (email,))
+APPLICATION = endpoints.api_server([RESTApi])
--- testtodolist007/js/main.js
+++ testtodolist007/js/main.js
@@ -5,19 +5,19 @@ var App = angular.module('App', ['ngRoute']);
 		$routeProvider
 			// route for the home page
 			.when('/', {
-				templateUrl : 'pages/home.html',
+				templateUrl : 'home.html',
 				controller  : 'mainController'
 			})
 
 			// route for the about page
 			.when('/about', {
-				templateUrl : 'pages/about.html',
+				templateUrl : 'about.html',
 				controller  : 'aboutController'
 			})
 
 			// route for the contact page
 			.when('/contact', {
-				templateUrl : 'pages/contact.html',
+				templateUrl : 'contact.html',
 				controller  : 'contactController'
 			})
             .otherwise({
