4ee8307e5519571183794b6520ad1b72f28707fc
Derek Wene <djrobotfreak@gmail.com>
2014-05-14 02:10:17 -0500

    finished restful part

--- testtodolist007/controller.py
+++ testtodolist007/controller.py
@@ -4,6 +4,7 @@ from protorpc import message_types
 from protorpc import remote
 import json
 from google.appengine.ext import ndb
+import collections
 
 # TODO: Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
@@ -17,35 +18,57 @@ class Response(messages.Message):
     """Greeting that stores a message."""
     message = messages.StringField(1)
 
-class listItem(ndb.Model):
-    title = ndb.StringProperty()
+
+class listItem:
+    id = 0
+
+    def __init__(self, title):
+        self.title = title
+        self.id = listItem.id
+        listItem.id += 1
 
 
-@endpoints.api(name='todolist', version='v1',
-               allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
-                                   IOS_CLIENT_ID],
+ITEMLIST = [listItem('take out the trash'), listItem('buy groceries')]
+
+
+def itemListToJSON():
+    itemlist = []
+    for item in ITEMLIST:
+        d = collections.OrderedDict()
+        d['title'] = item.title
+        d['id'] = item.id
+        itemlist.append(d)
+    return json.dumps(itemlist)
+
+
+@endpoints.api(name='todolist', version='v1',allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID, IOS_CLIENT_ID],
                audiences=[ANDROID_AUDIENCE])
-
 class RESTApi(remote.Service):
-    @endpoints.method(message_types.VoidMessage, Response,
-                 path='getlist', http_method='GET',
-                 name='listItem.getList')
+    @endpoints.method(message_types.VoidMessage, Response, path='getlist', http_method='GET', name='listItem.getList')
     def greeting_get(self, request):
-       return listItem.query()
+        try:
+            return Response(message=itemListToJSON())
+        except:
+            return Response(message='Error with stupid json')
 
+    ADD_ITEM = endpoints.ResourceContainer(Response)
 
-    @endpoints.method(message_types.VoidMessage, Response,
-                      path='derekgreeting', http_method='GET',
-                      name='todolist.howdyDerek')
-    def greetings_derek(self, request):
-        return Response(message='howdy derek')
+    @endpoints.method(ADD_ITEM, Response,path='addItem', http_method='POST', name='listItem.addItem')
+    def add_item(self, request):
+        item = listItem(request.message)
+        ITEMLIST.append(item)
+        return Response(message=itemListToJSON())
 
-    @endpoints.method(message_types.VoidMessage, Response,
-                      path='addItem', http_method='POST',
-                      name='listItem.addItem')
-    def greetings_derek(self, request):
-        item = listItem(content=request.message)
-        return Response(message=request.message)
+    ID_RESOURCE = endpoints.ResourceContainer(message_types.VoidMessage, id=messages.IntegerField(1,
+                                              variant=messages.Variant.INT32))
+
+    @endpoints.method(ID_RESOURCE, Response, path='removeItem/{id}', http_method='DELETE', name='listItem.removeItem')
+    def remove_item(self, request):
+        for item in ITEMLIST:
+            if item.id == request.id:
+                ITEMLIST.remove(item)
+                break
+        return Response(message=itemListToJSON())
 
 
 APPLICATION = endpoints.api_server([RESTApi])
